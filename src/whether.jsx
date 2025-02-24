b5e465dd9b1be874bcb2a52d3986d9e933dab911
surajjagtap9836 <surajjagtap9836@gmail.com>
2025-02-24 18:37:26 +0530

    add mui

--- src/whether.jsx
+++ src/whether.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-
-
-
+import './Whether.css'
 
 const Whether = () => {
+  const [city,setcity]=useState("");
+  let ip=(event)=>{
+    setcity(event.target.value);
+  }
+  let formdata=(event)=>{
+    event.preventDefault();
+    setcity("");
+  }
+
   return (<>
-    <div>
-      <h1>whether app</h1>
+    <div className='searchbox'>
+      <h1 className='title'>whether app</h1>
+      <form onSubmit={formdata}>
+      <TextField id="outlined-basic" label="City Name" variant="outlined" required value={city} onChange={ip}/><br/><br/>
+      <Button variant="contained"type='submit' >
+        Search
+      </Button>
+
+
       
-       
+      </form>
       
     </div>
     </> )
